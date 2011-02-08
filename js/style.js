8a4af381c74c2bc162191f881e15a3538e4605ed
Francesco <f@mazzo.li>
2011-02-08 15:09:27 +0000

    Added more effects.. max is not happy.

--- js/style.js
+++ js/style.js
@@ -1,15 +1,26 @@
 // Various functions relative to the styling of the page.
 
+var shadowHeight = 6;
+
 window.addEvent('domready', function() {
     var title = $$('a')[0];
 
-    var rand = Math.random();
-    if (rand >= 0.66) {
+    var rand = Math.floor(Math.random() * 4);
+    switch (rand) {
+    case 0:
         colorsTrans(title);
-    } else if (rand >= 0.33) {
+        break;
+    case 1:
         colorsRun(title);
-    } else {
-        colorBlink(title);
+        break;
+    case 2:
+        colorsBlink(title);
+        break;
+    case 3:
+        shadowColor(title);
+        break;
+    default:
+        break;
     }
 });
 
@@ -31,19 +42,10 @@ function colorsTrans(el) {
 
 function colorsRun(el) {
     var colors = [];
-    var spans = [];
-
-    for (var i = 0; i < el.get('text').length; i++) {
-        colors[i] = nextColor();
-        spans[i] = new Element('span', {
-            text: el.get('text').charAt(i)
-        });
-    }
-
-    el.empty();
+    var spans = spannify(el);
 
     for (var i = 0; i < spans.length; i++) {
-        el.grab(spans[i]);
+        colors[i] = nextColor(200);
     }
 
     var changeColors = function() {
@@ -51,8 +53,8 @@ function colorsRun(el) {
             spans[i].setStyle('color', colors[i]);
         }
 
-        var c = colors.shift();
-        colors.push(nextColor());
+        colors.shift();
+        colors.push(nextColor(200));
 
         setTimeout(changeColors, 100);
     };
@@ -60,7 +62,7 @@ function colorsRun(el) {
     changeColors();
 }
 
-function colorBlink(el) {
+function colorsBlink(el) {
     var colTween = new Fx.Tween(el, {
         duration: '200'
     });
@@ -82,14 +84,103 @@ function colorBlink(el) {
     blackTween.start('color', document.body.getStyle('background-color'));
 }
 
-function nextColor() {
-    var r = Math.floor(Math.random() * 256)
-    var g = Math.floor(Math.random() * 256)
-    var b = Math.floor(Math.random() * 256)
+function shadowColor(el) {
+    var colors = [];
 
-    if (r + g + b > 200) {
-        return nextColor();
+    for (var i = 0; i < shadowHeight; i++) {
+        colors[i] = nextColor();
     }
 
-    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    var colorShadow = function() {
+        colors.shift();
+        colors.push(nextColor());
+
+        var shadow = '1px 1px 0px black';
+
+        for (var i = 0; i < shadowHeight; i++) {
+            shadow += ', ' + (i + 2) + 'px ' + (i + 2) + 'px 0px ' + colors[i]; 
+        }
+
+        el.setStyle('text-shadow', shadow);
+
+        setTimeout(colorShadow, 100);
+    };
+
+    colorShadow();
 }
+
+function spannify(el) {
+    var spans = [];
+    
+    for (var i = 0; i < el.get('text').length; i++) {
+        spans[i] = new Element('span', {
+            text: el.get('text').charAt(i)
+        });
+    }
+
+    el.empty();
+
+    for (var i = 0; i < spans.length; i++) {
+        el.grab(spans[i]);
+    }
+
+    return spans;
+}
+
+function nextColor(treshold_up, treshold_down) {
+    if (treshold_up == undefined) {
+        treshold_up = 256;
+    }
+    if (treshold_down == undefined) {
+        treshold_down = 0;
+    }
+
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    if (r + g + b > treshold_up || r + g + b < treshold_down) {
+        return nextColor(treshold_up, treshold_down);
+    }
+
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+
+// function shadowHeights(el) {
+//     var spans = [];
+
+//     for (var i = 0; i < el.get('text').length; i++) {
+//         spans[i] = new Element('span', {
+//             text: el.get('text').charAt(i)
+//         });
+//     }
+
+//     el.empty();
+
+//     el.setStyle('text-shadow', 'none');
+
+//     for (var i = 0; i < spans.length; i++) {
+//         el.grab(spans[i]);
+//     }        
+
+//     var buildShadow = function(height) {
+//         var shadow = '1px 1px 0px black';
+        
+//         for (var i = 0; i < height; i++) {
+//             shadow += ', ' + (i + 2) + 'px ' + (i + 2) + 'px ' + '0px #476871';
+//         }
+
+//         return shadow;
+//     };
+    
+//     var changeHeights = function() {
+//         for (var i = 0; i < spans.length; i++) {
+//             spans[i].setStyle('text-shadow',
+//                               buildShadow(Math.floor(Math.random() * (shadowHeight + 1))));
+//         }        
+//         setTimeout(changeHeights, 300);
+//     }
+
+//     changeHeights();
+// }
