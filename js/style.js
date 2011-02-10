39e3e40f56a810f2da7392823460045db121306a
Francesco <f@mazzo.li>
2011-02-10 01:07:59 +0000

    Refixed the bug I tought I had fixed - whatthecommit.com

--- js/style.js
+++ js/style.js
@@ -29,10 +29,10 @@ function getEffects() {
                 Cookie.write(cookieName, JSON.stringify(list));
                 return effects[ev];
             } else {
-                fail();
+                return fail();
             }
         } else {
-            fail();
+            return fail();
         }
 
     } else {
