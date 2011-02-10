4e3c96a92b6cc2d95b841989541be04a69e7f98c
Francesco <f@mazzo.li>
2011-02-10 19:35:56 +0000

    Added full urls to disqus.

--- hakyll.hs
+++ hakyll.hs
@@ -1,9 +1,12 @@
 import Control.Monad (liftM)
+import Control.Monad.Reader (asks)
 import Control.Arrow ((>>>))
 import Data.List (sort)
 import Data.Either (Either(..))
+import System.FilePath (joinPath)
 
 import Text.Hakyll (hakyll)
+import Text.Hakyll.HakyllMonad (HakyllConfiguration (..))
 import Text.Hakyll.File (directory, getRecursiveContents)
 import Text.Hakyll.Render (css, static, renderChain)
 import Text.Hakyll.CreateContext (createPage, createListing)
@@ -15,12 +18,15 @@ main = hakyll "http://mazzo.li/b/" $ do
   directory static "js"
   
   articlesPaths <- liftM (reverse . sort) $ getRecursiveContents "articles"
+  absurl <- asks absoluteUrl
   let stripQuotes ""      = ""
       stripQuotes (c : s) | c == '\''  = "\\'" ++ stripQuotes s
                           | otherwise = c : stripQuotes s
-      articlesPages = map ((>>> renderDate "prettydate" "%b %e, %Y" "Date unknown") .
-                           (>>> renderValue "path" "identifier" stripQuotes) .
-                           createPage) articlesPaths
+      articlesPages = map (
+        (>>> renderDate "prettydate" "%b %e, %Y" "Date unknown") .
+        (>>> renderValue "path" "identifier" stripQuotes) .
+        (>>> renderValue "url" "absurl" (\u -> joinPath [absurl, u])) .
+        createPage) articlesPaths
 
   mapM_ (renderChain ["templates/article.html", "templates/default.html"])
     articlesPages
