65337e774b411589345f05a5b086aa109e11f065
Francesco Mazzoli <f@mazzo.li>
2021-10-24 10:38:31 +0200

    shorten titles

--- site.hs
+++ site.hs
@@ -39,7 +39,7 @@ main = hakyll $ do -- Assets
   create ["archive.html"] $ do
     route idRoute
     let archiveCtx =
-          field "posts" (\_ -> postList recentFirst) <> constField "title" "index" <> defaultContext
+          field "posts" (\_ -> postList recentFirst) <> constField "title" "blag — index" <> defaultContext
     compile $
       makeItem "" >>=
       loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
